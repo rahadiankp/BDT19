15063cd0f317b2c2090cca11a5dedaeaf4331e00
Rahadian <rahadiankp@users.noreply.github.com>
2019-11-25 01:45:39 +0700

    delete bugfix

--- wordpress-redis/fix_predis.ps1
+++
@@ -1,3 +0,0 @@
-echo "Fixing Redis Object Cache plugin..."
-docker-compose exec web sed -i "s/return 'INFO';/return 'info';/g" /var/www/html/wp-content/plugins/redis-cache/includes/predis/src/Command/ServerInfo.php
-echo "OK"
